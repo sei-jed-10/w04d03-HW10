57b7d1279ac25ad1bad67569a1b86257b0174d1e
KOSaber <k.o.saber@hotmail.com>
2019-12-14 19:25:09 +0300

    done with bonus

--- MBTA.rb
+++ MBTA.rb
@@ -8,18 +8,25 @@ class Subway
      @stops = 0
    @red =  Line.new('red',["South Station","Kendall","Central","Harvard","Park Street","Porter","Davis","Alewife"]) 
    @green = Line.new('green',["Government Center","Boylston","Arlington","Park Street","Copley","Hynes","Kenmore"]) 
-  @orange = Line.new('orange',["North Station","Haymarket","State","Downtown Crossing","Chinatown","Park Street","Back Bay","Forest Hills"]) 
-    @lines = [@red, @green, @orange]
+   @orange = Line.new('orange',["North Station","Haymarket","State","Downtown Crossing","Chinatown","Park Street","Back Bay","Forest Hills"]) 
+  
   end
-  def stops_between_stations(start_line, start_station, end_line, end_station)
-  @lines.map do |line| 
-    if line.name == start_line
-    @start_line = line
-    end
-    if line.name == end_line
-      @end_line = line
-    end
-  end
+  def stops_between_stations()#start_line, start_station, end_line, end_station)
+    puts "Please Enter StartLine: "
+    start_line = gets.chomp
+     
+    puts "Please Enter StartStation : "
+    start_station = gets.chomp
+    
+    puts "Please Enter EndLine : "
+    end_line = gets.chomp
+   
+    puts "Please Enter EndStation : "
+    end_station = gets.chomp
+
+  @start_line = self.instance_variable_get("@#{start_line}")
+  @end_line = self.instance_variable_get("@#{end_line}")
+  
     if @start_line == @end_line  
       Line.same_count_index(@start_line, start_station, end_station)
     else
@@ -37,11 +44,20 @@ class Line
        end
        def self.same_count_index(line, start_station, end_station)
         stops = ( line.stations.index(end_station) - line.stations.index(start_station) ).abs
-        puts "#{stops} stops"
+            # bouns       
+         puts "Rider Transfers from #{@start_line} line from #{start_station}"
+         puts "Rider Arrives to #{@end_line} line At #{end_station} "
+         puts "#{stops} stops"
        end
        def self.deff_count_index(start_line, start_station, end_line, end_station)
         stops = (end_line.stations.index(end_station) - end_line.stations.index('Park Street')).abs
         stops = stops + (start_line.stations.index('Park Street') - start_line.stations.index(start_station)).abs
+            # bouns       
+        puts "Rider Transfers from #{@start_line} line from #{start_station}"
+        if startline != endline
+         puts "Change at Park Street. "	
+        end
+        puts "Rider Arrives to #{@end_line} line At #{end_station} "
         puts "#{stops} stops"
        end
 end
@@ -53,6 +69,6 @@ class Station
   end
 end
 mbta = Subway.new
-mbta.stops_between_stations('red', 'Alewife', 'red', 'Park Street') # 3 stops
-mbta.stops_between_stations('red', 'Alewife', 'orange', 'Downtown Crossing') # 5 stops
-mbta.stops_between_stations('red', 'South Station', 'green', 'Kenmore') # 7 stops
+mbta.stops_between_stations()#'red', 'Alewife', 'red', 'Park Street') # 3 stops
+# mbta.stops_between_stations('red', 'Alewife', 'orange', 'Downtown Crossing') # 5 stops
+# mbta.stops_between_stations('red', 'South Station', 'green', 'Kenmore') # 7 stops
